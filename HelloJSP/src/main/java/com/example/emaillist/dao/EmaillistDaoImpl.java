10fed9da745ad31fab2f065b96c07f9d81757d4d
서지원 <qrd7079@gmail.com>
2021-08-15 16:21:47 +0900

    복습내용 주석 추가

--- HelloJSP/src/main/java/com/example/emaillist/dao/EmaillistDaoImpl.java
+++ HelloJSP/src/main/java/com/example/emaillist/dao/EmaillistDaoImpl.java
@@ -13,6 +13,7 @@ import java.util.List;
 import com.example.emaillist.vo.EmailVo;
 
 public class EmaillistDaoImpl implements EmaillistDao {
+	
 	private Connection getConnection() throws SQLException {
 		Connection conn = null;
 
@@ -29,7 +30,7 @@ public class EmaillistDaoImpl implements EmaillistDao {
 		}
 
 		return conn;
-	}
+	}// private Connection getConnection() throws SQLException {}  end
 
 	@Override
 	public List<EmailVo> getList() {
@@ -41,9 +42,10 @@ public class EmaillistDaoImpl implements EmaillistDao {
 
 		try {
 			conn = getConnection();
-			stmt = conn.createStatement();
+			stmt = conn.createStatement(); //데이터베이스로 SQL 문을 보내기 위한 SQLServerStatement 개체를 만듦.
 
-			String sql = "SELECT no, last_name, first_name, email, createdAt " + " FROM emaillist ORDER BY no DESC";
+			String sql = "SELECT no, last_name, first_name, email, createdAt " + 
+						" FROM emaillist ORDER BY no DESC";
 
 			// 쿼리 수행
 			rs = stmt.executeQuery(sql);
@@ -80,7 +82,8 @@ public class EmaillistDaoImpl implements EmaillistDao {
 			}
 		}
 		return list;
-	}
+	} //public List<EmailVo> getList() end
+	
 
 	@Override
 	public int insert(EmailVo vo) {
@@ -97,12 +100,56 @@ public class EmaillistDaoImpl implements EmaillistDao {
 					" VALUES(seq_emaillist_pk.NEXTVAL, ?, ?, ?)";
 			pstmt = conn.prepareStatement(sql);
 			//	파라미터 바인딩
-			pstmt.setString(1, vo.getLastName());
-			pstmt.setString(2, vo.getFirstName());
-			pstmt.setString(3, vo.getEmail());
+			pstmt.setString(1, vo.getLastName()); // 첫번째 ?의 값을 지정
+			pstmt.setString(2, vo.getFirstName()); // 두번째 ?의 값을 지정
+			pstmt.setString(3, vo.getEmail()); // 세번째 ?의 값을 지정
 			
 			//	쿼리 수행
 			count = pstmt.executeUpdate();
+			
+//			<execute, executeQuery, executeUpdate 메서드>
+//			JDBC Type 4 드라이버는 쿼리를 실행 할 수 있도록 execute, executeQuery, executeUpdate 3개의 메서드를 제공
+//
+//			1. execute
+//			execute 메서드는 모든 유형의 SQL 문장과 함께 사용할 수 있으며, boolean 값을 반환합니다. 
+//			반환 값이 ‘true’이면, getResultSet 메서드를 사용함으로써 결과 집합을 얻을 수 있습니다. 
+//			반대로 반환 값이 ‘false’이면, 업데이트 개수 또는 결과가 없는 경우입니다.
+//
+//			execute 메서드는 Select, Insert, Update, Delete, DDL 문을 모두 실행할 수 있는 특징이 있습니다.
+//
+//			execute 메서드
+//			public boolean execute(String sql) throws SQLException;
+//			반환 값
+//			true : result
+//			false : update count or no result
+			
+			
+//			2. executeUpdate
+//			executeUpdate 메서드는 데이터베이스에서 데이터를 추가(Insert), 삭제(Delete), 수정(Update)하는 SQL 문을 실행
+//			메서드의 반환 값은 해당 SQL 문 실행에 영향을 받는 행 수를 반환합니다.
+//
+//			데이터베이스 개발 툴(SQLGate, Toad, Sequel Pro)를 사용하다 보면, 
+//			쿼리 실행 후 메시지로 affectedRows 값을 사용자에게 보여줍니다.
+//			affectedRows 값은 사용자가 수행한 DML 문이 영향을 끼친 행 수를 나타내는데, 바로 executeUpdate 메서드의 반환 값입니다.
+//
+//			executeUpdate 메서드
+//			public int executeUpdate(String sql) throws SQLException;
+//			반환 값
+//			int : row count
+			
+			
+//			3. executeQuery
+//			executeQuery 메서드는 데이터베이스에서 데이터를 가져와서 결과 집합을 반환합니다. 
+//			이 메서드는 Select 문에서만 실행하는 특징이 있습니다.
+//
+//			executeQuery 메서드
+//			public ResultSet executeQuery(String sql) throws SQLException;
+//			반환 값
+//			ResultSet : object that contains the data produced by the given query
+//
+//			출처: https://lelecoder.com/2 [lelecoder]
+			
+			
 		} catch (SQLException e) {
 			e.printStackTrace();
 		} finally {
@@ -114,7 +161,7 @@ public class EmaillistDaoImpl implements EmaillistDao {
 			}
 		}
 		return count;
-	}
+	}// public int insert(EmailVo vo) end
 
 	@Override
 	public int delete(Long pk) {
@@ -143,6 +190,7 @@ public class EmaillistDaoImpl implements EmaillistDao {
 			}
 		}
 		return deletedCount;
-	}
+	} // public int delete(Long pk) end
+	
 
 }
